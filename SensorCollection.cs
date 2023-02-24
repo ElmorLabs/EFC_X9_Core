e11c9321f3030300c2ae513d3d701e084fe00495
Jon Sandström <jon@elmorlabs.com>
2023-02-24 16:22:56 +0800

    Fix IsValid bug

--- SensorCollection.cs
+++ SensorCollection.cs
@@ -8,15 +8,15 @@ namespace EFC_Core
 {
     public class SensorCollection
     {
-        public double Temperature1;
-        public double Temperature2;
-        public double TemperatureAmbient;
-        public double Humidity;
-        public double ExternalFanDuty;
-        public double FanVoltage;
-        public double FanCurrent;
-        public double FanPower;
-        public double[] FanSpeeds = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        public double[] FanDuties = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        public double? Temperature1;
+        public double? Temperature2;
+        public double? TemperatureAmbient;
+        public double? Humidity;
+        public double? ExternalFanDuty;
+        public double? FanVoltage;
+        public double? FanCurrent;
+        public double? FanPower;
+        public double?[] FanSpeeds = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        public double?[] FanDuties = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     }
 }
